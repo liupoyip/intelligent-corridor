4237ec957db1a0b3239f0ac2a9d767cd4b741c2d
liupoyip <liupoyip@gmail.com>
2023-07-12 15:20:33 +0800

    LGTM!!

--- read_continuous_rawdata.py
+++ read_continuous_rawdata.py
@@ -1,64 +1,82 @@
 import os
-import time
+from datetime import datetime
 import json
+from typing import Optional
 
 import mysql.connector
 import mysql.connector.cursor
-from typing import Optional
+import pandas as pd
 
 
-class MySQLRawdataReader:
+db = None
+cursor: Optional[mysql.connector.cursor.MySQLCursor] = None
+host = None
+port = None
+user = None
+password = None
+db_name = None
+rawdata_table_name = None
+headers = None
+data_types = None
+current_data: tuple = None
+rawdata_table_name = None
+cfg = None
 
-    db = None
-    cursor: Optional[mysql.connector.cursor.MySQLCursor] = None
-    host = None
-    port = None
-    user = None
-    password = None
-    db_name = None
-    rawdata_table_name = None
-    headers = None
-    data_types = None
-    current_data: tuple = None
-    rawdata_table_name = None
-    cfg = None
-
-    target_dir = None
-    database_dir = None
-
-    def __init__(self, cfg):
-        self.read_cfg()
-
-    def read_cfg(self):
-        self.host = cfg['host']
-        self.port = cfg['port']
-        self.user = cfg['user']
-        self.password = cfg['password']
-        self.db_name = cfg['machine']
-        self.rawdata_table_name = cfg['rawdata_table_name']
-        self.headers = cfg['headers']
-        self.task_name = cfg['task']
-
-    def connect_to_server(self):
-        print('connect to mysql server...')
-        print(f'host : {self.host}:{self.port}')
-        print(f'user : {self.user}')
-        self.db = mysql.connector.connect(
-            host=self.host,
-            port=self.port,
-            user=self.user,
-            password=self.password)
-
-    def use_database(self):
-        print(f'execute SQL command: {sql}')
-        sql = f'USE {self.db_name}'
-        self.cursor = self.db.cursor()
-        self.cursor.execute(sql)
-
+target_dir = None
+database_dir = None
 
 uploader_cfg_path = 'cfg.json'
 with open(uploader_cfg_path, 'r') as file:
     cfg = json.load(file)
-rawdata_reader = MySQLRawdataReader(cfg=cfg)
-rawdata_reader.connect_to_server()
-rawdata_reader.use_database()
+
+host = cfg['host']
+port = cfg['port']
+user = cfg['user']
+password = cfg['password']
+db_name = cfg['machine']
+rawdata_table_name = cfg['rawdata_table_name']
+headers = cfg['headers']
+task_name = cfg['task']
+
+# def connect_to_server():
+print('connect to mysql server...')
+print(f'host : {host}:{port}')
+print(f'user : {user}')
+db = mysql.connector.connect(
+    host=host,
+    port=port,
+    user=user,
+    password=password)
+
+# use_database():
+sql = f'USE {db_name}'
+print(f'execute SQL command: {sql}')
+cursor = db.cursor()
+cursor.execute(sql)
+
+sql = f'Select * from {rawdata_table_name}'
+rawdata_store = pd.read_sql(sql, db)
+db.close()
+
+print(rawdata_store)
+# connect_to_server()
+# use_database()
+
+# dummy time range
+time_format = '%Y%m%dT%H%M%S'
+str_start_time = '20230710T145000'
+str_end_time = '20230712T145100'
+start_time = datetime.strptime(str_start_time, time_format)
+end_time = datetime.strptime(str_end_time, time_format)
+header_start_time = headers[1]
+header_record_cfg_path = headers[2]
+
+rawdata_store[header_start_time] = pd.to_datetime(
+    rawdata_store[header_start_time])
+time_mask = (rawdata_store[header_start_time] > start_time) & (
+    rawdata_store[header_start_time] <= end_time)
+print(time_mask)
+
+masked_rawdata_store = rawdata_store.loc[time_mask]
+print(masked_rawdata_store)
+masked_rawdata_store = rawdata_store.loc[time_mask]
